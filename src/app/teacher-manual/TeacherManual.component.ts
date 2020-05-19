062a44d12455ea59e12177024ea968e37b50ca9c
lvduarte <lucvduarte@gmail.com>
2020-05-18 21:42:14 -0300

    adicao do conteudo do manual de professores

--- src/app/teacher-manual/TeacherManual.component.ts
+++ src/app/teacher-manual/TeacherManual.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './teacher-manual.component.html'
+    templateUrl: './teacher-manual.component.html',
+    styleUrls: ['./teacher-manual.component.css']
 })
 export class TeacherManual {
     title = 'app';
